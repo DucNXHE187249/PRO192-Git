5bf0ab6c990cc316ec3028a277a2698b24c0c445
DucNXHE187249 <DucNXHE187249@fpt.edu.vn>
2023-05-23 15:24:54 +0700

    23/05/2023

---
+++ Workshop#2/Part1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Part1 {
+    public static void main(String [] args) {
+        boolean cont = false;
+        do {
+            try {
+                int num;
+                Scanner sc = new;
+                System.out.println("Enter the number" );
+                if (num < 1) throw new Exception();
+                System.out.println("The number is: "+ num);
+                cont = false;
+            } catch(Exception e) {
+                System.out.println("The number is invalid");
+                cont = true;
+            }
+        } while (cont);
+    }
+}
