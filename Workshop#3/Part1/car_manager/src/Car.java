41b294f375b237a1e2b2b1726270eee3f0ac86f5
DucNXHE187249 <DucNXHE187249@fpt.edu.vn>
2023-06-02 12:14:26 +0700

    car_manager

--- Workshop#3/Part1/car_manager/src/Car.java
+++ Workshop#3/Part1/car_manager/src/Car.java
@@ -4,19 +4,23 @@ public class Car {
     boolean Convertible;
     boolean ParkingBrake;
 
+    //Constructors
     Car(){}
     Car(String Colour, int EnginePower, boolean Convertible, boolean ParkingBrake){}
 
+    //Getters
     public void setColour(String Colour) {this.Colour = Colour;};
     public void setConvertible(boolean Convertible) {this.Convertible = Convertible;};
     public void setEnginePower(int EnginePower) {this.EnginePower = EnginePower;};
     public void setParkingBrake(boolean ParkingBrake) {this.ParkingBrake = ParkingBrake;};
 
+    //Setters
     public String getColour() {return Colour;}
     public boolean getConvertible() {return Convertible;}
     public int getEnginePower() {return EnginePower;}
     public boolean getParkingBrake() {return ParkingBrake;}
 
+    //Other logic methods
     public void pressStartButton(){
         System.out.println("You've pressed the Start button !");
     }
